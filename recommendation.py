e1e921e3f35b4f5760a95051a06f5ffde22f3ced
Fay Elhassan <38003091+FayElhassan@users.noreply.github.com>
2023-08-25 10:32:13 +0000

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -10,11 +10,9 @@ user_item_similarity = None
 
 # Load the 'en_core_web_sm' model only if it's not present
 
-try:
-    nlp = spacy.load('en_core_web_sm')
-except:
-    spacy.cli.download('en_core_web_sm')
-    nlp = spacy.load('en_core_web_sm')
+# if 'en_core_web_sm' not in spacy.util.get_installed_models():
+    # spacy.cli.download("en_core_web_sm")
+nlp = spacy.load('en_core_web_sm')
 
 
 
